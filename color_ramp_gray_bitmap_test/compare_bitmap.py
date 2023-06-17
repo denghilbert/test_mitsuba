20d8166df919ff82a32ebb35f9875a61568fad79
denghilbert <971431670@qq.com>
2023-06-17 13:18:37 +0200

    Add TEST ColorRamp gray bitmap factor

--- color_ramp_gray_bitmap_test/compare_bitmap.py
+++ color_ramp_gray_bitmap_test/compare_bitmap.py
@@ -4,7 +4,7 @@ import os
 os.environ["OPENCV_IO_ENABLE_OPENEXR"]="1"
 
 blender = cv2.imread('./0000.exr', cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
-mitsuba = cv2.imread('./interpolate_middle_red_blue.exr', cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+mitsuba = cv2.imread('./interpolate_gray_bitmap_factor.exr', cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
 
 err = cv2.absdiff(blender, mitsuba)
 
--- color_ramp_gray_bitmap_test/export_bitmap_factor.sh
+++ color_ramp_gray_bitmap_test/export_bitmap_factor.sh
@@ -3,7 +3,7 @@
 blender -b ./test_inter_bitmap.blend -o ./ -F OPEN_EXR -f 0
 echo "Finish Blender rendering"
 
-~/Desktop/mitsuba3/build/mitsuba -o ./interpolate_middle_red_blue.exr -m cuda_ad_rgb ./test_inter_bitmap.xml
+~/Desktop/mitsuba3/build/mitsuba -o ./interpolate_gray_bitmap_factor.exr -m cuda_ad_rgb ./test.xml
 echo "Finish Mitsuba rendering"
 
 python ./compare_bitmap.py
