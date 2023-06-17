35eecfa5ea555998d973a9e1f2292e800d9cfd5f
denghilbert <971431670@qq.com>
2023-06-17 13:17:52 +0200

    Add TEST ColorRamp bitmap or single factor

---
+++ color_ramp_bitmap_single_test/compare_single.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+import os
+os.environ["OPENCV_IO_ENABLE_OPENEXR"]="1"
+
+blender = cv2.imread('./0000.exr', cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+mitsuba = cv2.imread('./interpolate_middle_red_blue.exr', cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+
+err = cv2.absdiff(blender, mitsuba)
+
+cv2.imwrite('./error.exr', err)
